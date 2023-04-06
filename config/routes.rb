801f0c9ebd6c44667c0c01f1e1ac96b4823886c9
dialite <raymondolisa01@gmail.com>
2023-04-06 07:59:51 +0100

    Create appointments resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :doctors, only: %i[index show create destroy]
+      resources :doctors, only: %i[index show create destroy] do
+        resources :appointment, only: %i[index show create destroy]
+      end
       post '/auth/signup', to: 'users#create'
     end
   end
