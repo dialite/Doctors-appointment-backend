145fd2450d0748380a057e324825e8ec085cb1c1
dialite <raymondolisa01@gmail.com>
2023-04-09 14:18:15 +0100

    Modify doctor model with validations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,11 @@
 class Doctor < ApplicationRecord
   has_many :appointments, class_name: 'Appointment', foreign_key: 'appointment_id', dependent: :destroy
   has_many :users, through: :appointments, dependent: :destroy
+
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :gender, presence: true
+  validates :speciality, presence: true
+  validates :experience, presence: true
+  validates :consultation, presence: true
 end
