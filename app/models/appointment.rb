36f18b2c008eed3f50ee428d1ccf7b6bd7140dca
dialite <raymondolisa01@gmail.com>
2023-04-09 08:36:56 +0100

    Validate columns in appointment table

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :doctor, class_name: 'Doctor', foreign_key: 'doctor_id'
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :datetime, presence: true
+  validates :user_id, presence: true
+  validates :doctor_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      get '/patient/appointments', to: 'users#appointments'
       resources :doctors, only: %i[index show create destroy] do
         resources :appointments, only: %i[index show create destroy]
       end
