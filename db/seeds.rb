561efa502d0b535fd3b91b0167e331051a8ee489
dialite <raymondolisa01@gmail.com>
2023-04-10 12:00:53 +0100

    Modify users table by adding and removing columns

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name: "Dr. Victor Wiel", email: 'codinggroup@gmail.com', password: 'password', password_confirmation: 'password', role: 'admin')
+
+Doctor.create(name: "Dr. Ruthie", lastname: "Brendan" speciality: 'Surgeon ', 
+    image: ActionController::Base.helpers.image_url('dr_andrew_bile.png'), gender: 'Female')
+Doctor.create(name: "Dr. Anne Zawadi", speciality: 'Dentist', 
+    image: ActionController::Base.helpers.image_url('dr_anne_zawadi.png'), gender: 'Female')
+Doctor.create(name: "Dr. Mark Xaverian", speciality: 'Oncologist', 
+    image: ActionController::Base.helpers.image_url('dr_mark_x.png'), gender: 'Male')
+Doctor.create(name: "Dr. Rawal Parag", speciality: 'Cardiologist', 
+    image: ActionController::Base.helpers.image_url('dr_rawal_parag.png'), gender: 'Male')
