9b538f51337ad956fa7eb14b3627ad4616f536b4
dialite <raymondolisa01@gmail.com>
2023-04-12 23:20:44 +0100

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: %i[index show create update destroy]
       resources :doctors, only: %i[index show create update destroy]
       resources :appointments, only: %i[index show create update destroy]
       # post '/auth/signup', to: 'users#create'
