151db24bf77b8978102a75657a24cfbe78e75433
dialite <raymondolisa01@gmail.com>
2023-04-12 22:34:43 +0100

    Modify doctor model rspecfile

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
   subject do
-    user = User.create(username: 'Boaz')
     Doctor.new(name: 'Raymond', lastname: 'Olisa', speciality: 'Surgeon',
                experience: 10, consultation: 200, image: 'https://doctorphoto.com/profile.png')
   end
