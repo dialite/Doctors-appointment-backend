c8acf526199f1634ce6f8624e243d3f2d4d81c5a
dialite <raymondolisa01@gmail.com>
2023-04-12 12:10:25 +0100

    Create appointment model test

---
+++ spec/models/appointment_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Appointment, type: :model do
+  let(:user) { User.create(username: 'Boaz') }
+  let(:doctor) { Doctor.create(name: 'Raymond', lastname: 'Olisa', speciality: 'Surgeon',
+      experience: 10, consultation: 200, image: 'https://doctorphoto.com/profile.png') }
+
+  subject do
+    Appointment.new(user: user, doctor: doctor, datetime: '2023-04-10T12:00:00.000Z', city: 'New-York')
+  end
+
+  before { subject.save }
+
+  context 'Test for validation : ' do
+    it 'Description should be present' do
+      subject.city = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Appointment datetime should be present' do
+      subject.datetime = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Appointment_user should be present' do
+      subject.user_id = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Appointment_doctor should be present' do
+      subject.doctor_id = nil
+      expect(subject).to_not be_valid
+    end
+  end
+end
