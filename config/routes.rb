e83490c40beb150ee2a25fe1ee1b855020ee319e
dialite <raymondolisa01@gmail.com>
2023-04-13 15:47:36 +0100

    Install rswag gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
   resources :appointments
   resources :doctors
   resources :users
